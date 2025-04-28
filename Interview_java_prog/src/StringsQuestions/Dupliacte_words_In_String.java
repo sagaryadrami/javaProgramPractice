6f67ad1e17ccb225298d2b2732dc8620dd419768
Sagar Yadrami <sagaryadrami@gmail.com>
2025-04-28 23:54:05 +0530

    new str questions from laptop_1

---
+++ Interview_java_prog/src/StringsQuestions/Dupliacte_words_In_String.java
@@ -0,0 +1,24 @@
+package StringsQuestions;
+
+import java.util.HashMap;
+
+public class Dupliacte_words_In_String {
+
+	public static void main(String[] args) {
+		String sentense="sagar is learns string problems";
+		duplicateWordInAString(sentense);
+	}
+	
+	public static void duplicateWordInAString(String Inpstr) {
+		
+		HashMap<String ,Integer> count = new HashMap<>();
+		for(String w :Inpstr.split(" ")) {
+			if(count.containsKey(w)) {
+				count.put(w, count.get(w)+1);
+			}else {
+				count.put(w, 1);
+			}
+		}
+		System.out.println(count);
+	}
+}
